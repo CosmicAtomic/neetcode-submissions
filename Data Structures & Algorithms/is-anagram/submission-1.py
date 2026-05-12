55fd5fd6bfc7249d46d369eec7643e1e72d744e3
Moses Ogunade <mosess.ogunade@gmail.com>
2026-05-12 09:11:30 +0100

    Simplify isAnagram method using sorted

--- Data Structures & Algorithms/is-anagram/submission-1.py
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -2,16 +2,6 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        for char in s:
-            if char not in t:
-                return False
-            if s.count(char) != t.count(char):
-                return False
-        for char in s:
-            if char not in s:
-                return False
-            if s.count(char) != t.count(char):
-                return False
-        return True
+        return sorted(s) == sorted(t)
         
         
