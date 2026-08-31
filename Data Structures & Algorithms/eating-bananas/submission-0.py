983b2d78812d28adfe3ec93d23c64b7ae9812fdb
OGUNADE, Moses Ayomide <mosess.ogunade@gmail.com>
2026-08-31 05:04:03 +0100

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,27 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
+        lo, hi = 1, max(piles)
+        while lo <= hi:
+            k = lo + (hi - lo)//2
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(p/k)
+            if totalTime <= h:
+                res = k
+                hi = k - 1
+            else:
+                lo = k + 1
+        return res
+
+
+
+                
+
+
+
+
+
+        
+        
